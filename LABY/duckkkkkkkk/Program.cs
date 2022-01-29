c50eb40ad8e3c960910ff566f3e3c31a4aa46f35
Tesl1k <danildavydov161@mail.ru>
2022-01-29 09:04:09 +0300

    123

---
+++ LABY/duckkkkkkkk/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Ducks;
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duckk[] duck = new Duckk[] { new MallardDuck(), new RedheadDuck() };
+
+            foreach (Duckk look in duck)
+            {
+                Console.WriteLine(look.swim());
+                Console.WriteLine(look.quack());
+                Console.WriteLine(look.display());
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ LABY/dukkkkks/RedHeadDuck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ducks
+{
+    public class RedheadDuck : Duckk
+    {
+        public override string display()
+        {
+            return "display " + this.GetType();
+        }
+    }
+}
